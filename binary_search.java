ebbf578e48f6549a153a5a1534c8b1656e58e934
Adian <156452746+adiancodes@users.noreply.github.com>
2024-08-12 23:33:48 +0530

    Update binary_search.java

--- binary_search.java
+++ binary_search.java
@@ -16,6 +16,9 @@ public class binary_search {
         System.out.println("Enter the element to search ");
         int target = input.nextInt();
 
+        //to check if the array is ascending order or descending order
+
+
         int ans = bsearch(arr,target);
         System.out.println(ans);
 
@@ -24,22 +27,32 @@ public class binary_search {
     static int bsearch(int[] arr, int target){
         int start = 0 ;
         int end = arr.length-1;
+        boolean isAsc;
+        isAsc = arr[start] < arr[end];
         while(start <= end){
             //finding the middle element
             //int mid = (start + end )/2 but sometimes the size of end is so large due to start + end
             //so we use optimised
             int mid = start +(end - start)/2;
 
-            //conditions for searching
-
-            if (target > arr[mid]){
-                start = start +1;
+            if ( arr[mid] == target ){
+                return mid;
             }
-            else if ( target < arr[mid]){
-                end = end -1;
+
+            //conditions for searching
+            if (isAsc){
+                if (target > arr[mid]) {
+                    start = start + 1;
+                } else {
+                    end = end - 1;
+                }
             }
             else{
-                return mid;
+                if (target < arr[mid]) {
+                    start = start + 1;
+                } else {
+                    end = end - 1;
+                }
             }
         }
         return -1;
